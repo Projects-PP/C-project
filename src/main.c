a0e54e15978c76062bae61bcf96a45c9f2ad0fdc
kvrtsll <221159445+kvrtsll@users.noreply.github.com>
2026-06-14 16:23:24 +0200

    small changes in game event handling

--- src/main.c
+++ src/main.c
@@ -1,57 +1,64 @@
-#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
-#include "../include/gui.h"
-#include "../include/game_logic.h"
-#include "../include/file_operations.h"
+#include "game_logic.h"
+#include "gui.h"
 
 int main(void) {
     ChessState state;
     setup_board(&state);
     init_history(&state);
 
-    if (!init_gui()) return 1;
+    if (!init_gui()) {
+        return 1;
+    }
 
     int running = 1;
-    int selected_x = -1, selected_y = -1;
+    SDL_Event e; //event tracker
+    int selected_x = -1;
+    int selected_y = -1;
 
     while (running) {
-        SDL_Event e;
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) running = 0;
-
+            if (e.type == SDL_QUIT) {
+                running = 0;
+            }
             if (e.type == SDL_MOUSEBUTTONDOWN) {
                 int action = handle_click(e.button.x, e.button.y);
-
                 if (action >= 0) {
                     int x = action % 8;
                     int y = action / 8;
-
-                    if (selected_x == -1)
-                        selected_x = x, selected_y = y;
-                    else {
-                        if (apply_move(&state, selected_x, selected_y, x, y))
+                    if (selected_x == -1) {
+                        if (state.board[y][x].type != EMPTY && state.board[y][x].color == state.turn) {
+                            selected_x = x;
+                            selected_y = y;
+                        }
+                    } else {
+                        if (apply_move(&state, selected_x, selected_y, x, y)) {
                             push_history(&state);
-
-                        selected_x = selected_y = -1;
+                        }
+                        selected_x = -1;
+                        selected_y = -1;
                     }
-                }
-                else if (action == BTN_NEW_GAME) {
+                } else if (action == BTN_NEW_GAME) {
                     setup_board(&state);
                     init_history(&state);
-                }
-                else if (action == BTN_UNDO) {
+                    selected_x = -1;
+                    selected_y = -1;
+                } else if (action == BTN_UNDO) {
                     undo_move(&state);
-                }
-                else if (action == BTN_REDO) {
+                    selected_x = -1;
+                    selected_y = -1;
+                } else if (action == BTN_REDO) {
                     redo_move(&state);
+                    selected_x = -1;
+                    selected_y = -1;
                 }
             }
         }
-
         render_game(&state, selected_x, selected_y);
         SDL_Delay(16);
     }
 
     cleanup_gui();
+    free_history();
     return 0;
 }
